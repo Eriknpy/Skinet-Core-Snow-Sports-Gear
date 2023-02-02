6916d21b85a3069abc92258c5daaa64e2898d291
Erik <101416162+Eriknpy@users.noreply.github.com>
2023-02-02 23:39:54 +0100

    create : StoreContext
    + Products DbSet

---
+++ SnowSportsGear/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SnowSportsGear.Entities;
+
+namespace SnowSportsGear.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
